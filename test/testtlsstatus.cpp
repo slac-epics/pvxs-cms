91f5d5f1134cd63d0da70df7f87cbaa88ca9516b
george-mcintyre <george@level-n.com>
2026-04-22 18:18:18 +0200

    refactor: move test helpers to cms namespace and drop empty using namespace pvxs::certs
    Migrate the pvxs::certs block in test/certcontext.h (holding test helpers like CertCtx, CounterMap, testStatusRequest, etc.) to the top-level cms namespace; leave the pvxs::tag and pvxs::server blocks unchanged because they serve as intentional namespace extensions. Remove the now-dead 'using namespace pvxs::certs;' directives from testtlsstatus.cpp, testtlswithcms.cpp, testtlswithcmsandstapling.cpp (the namespace is empty after Phase 6) and add narrow 'using cms::CertCtx; using cms::CounterMap;' at file scope so references remain unqualified at the call sites.
    
    (cherry picked from commit f99935c20f4c6cfe228a358938f8e059f8350ab4)

--- test/testtlsstatus.cpp
+++ test/testtlsstatus.cpp
@@ -37,6 +37,8 @@
 namespace {
 using namespace pvxs;
 using cms::ConfigCms;
+using cms::CertCtx;
+using cms::CounterMap;
 using cms::cert::CertStatus;
 using cms::cert::CmsStatusManager;
 using cms::cert::CertStatusFactory;
@@ -56,7 +58,6 @@ using cms::cert::OCSP_CERTSTATUS_GOOD;
 using cms::cert::OCSP_CERTSTATUS_REVOKED;
 using cms::cert::OCSP_CERTSTATUS_UNKNOWN;
 using cms::detail::ServerEv;
-using namespace pvxs::certs;
 
 struct Tester {
     // Pristine values
