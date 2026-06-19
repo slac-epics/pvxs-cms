7b6e108446aa84a2a896d88fc6affd50c927a201
Michael Davidsaver <mdavidsaver@gmail.com>
2026-06-19 14:46:27 -0700

    pvxcert --status-pv -f xxx.p12

--- src/tools/pvxcert/pvxcert.cpp
+++ src/tools/pvxcert/pvxcert.cpp
@@ -65,6 +65,7 @@ struct cliparams {
     bool deny = false;
     bool password_flag = false;
     bool debug = false;
+    bool only_status_pv = false;
     bool verbose = false;
 
     cliparams(int argc, char *argv[])
@@ -89,6 +90,7 @@ int readParameters(cliparams& params, client::Config &conf) {
     // Define flags
     app.add_flag("-h,--help", help);
     app.add_flag("-v,--verbose", params.verbose);
+    app.add_flag("--status-pv", params.only_status_pv);
     app.add_flag("-d,--debug", params.debug);
     app.add_flag("-p,--password", params.password_flag);
     app.add_flag("-V,--version", show_version);
@@ -147,6 +149,7 @@ int readParameters(cliparams& params, client::Config &conf) {
                   << "  (--cert-pv-prefix) <prefix>                Status PV prefix for the <issuer>:<serial> form.  Default CERT:STATUS:\n"
                   << "  (-d | --debug)                             Debug mode: Shorthand for $PVXS_LOG=\"pvxs.*=DEBUG\"\n"
                   << "  (-v | --verbose)                           Verbose mode\n"
+                  << "  (--status-pv)                              Print only certificate status PV name.  Use with -f\n"
                   << std::endl;
     }
 
@@ -188,7 +191,7 @@ int main(int argc, char *argv[]) {
         // Handle the flags after parsing
         if (params.debug) logger_level_set("pvxs.*", Level::Debug);
         if (params.password_flag) {
-            std::cout << "Enter password: ";
+            std::cerr << "Enter password: ";
 #if !defined(_WIN32) && !defined(_MSC_VER)
             setEcho(false);
 #endif
@@ -196,7 +199,7 @@ int main(int argc, char *argv[]) {
 #if !defined(_WIN32) && !defined(_MSC_VER)
             setEcho(true);
 #endif
-            std::cout << std::endl;
+            std::cerr << std::endl;
         }
 
         if (params.approve) {
@@ -211,7 +214,7 @@ int main(int argc, char *argv[]) {
 
         auto client = conf.build();
 
-        if (params.verbose) std::cout << "Effective config\n" << conf;
+        if (params.verbose) std::cerr << "Effective config\n" << conf;
 
         std::list<std::shared_ptr<client::Operation>> ops;
 
@@ -231,16 +234,19 @@ int main(int argc, char *argv[]) {
                 } catch (...) {
                 }
 
-                std::cout << "Certificate Details: " << std::endl
-                          << "============================================" << std::endl
-                          << ossl::ShowX509{cert_data.cert.get()} << std::endl
-                          << (config_id.empty() ? "" : "Config URI     : " + config_id + "\n") << "--------------------------------------------\n"
-                          << std::endl;
+                if(!params.only_status_pv)
+                    std::cout << "Certificate Details: " << std::endl
+                              << "============================================" << std::endl
+                              << ossl::ShowX509{cert_data.cert.get()} << std::endl
+                              << (config_id.empty() ? "" : "Config URI     : " + config_id + "\n") << "--------------------------------------------\n"
+                              << std::endl;
                 cert_id = certs::CmsStatusManager::getStatusPvFromCert(cert_data.cert);
             } catch (std::exception &e) {
-                std::cout << "Online Certificate Status: " << std::endl
+                std::cerr << "Online Certificate Status: " << std::endl
                           << "============================================" << std::endl
                           << "Not configured: " << e.what() << std::endl;
+                if(params.only_status_pv)
+                    return 1;
                 return 0;
             }
         } else {
@@ -248,6 +254,14 @@ int main(int argc, char *argv[]) {
             const auto ncolons = std::count(params.cert_status_pv.begin(), params.cert_status_pv.end(), ':');
             cert_id = ncolons >= 2 ? params.cert_status_pv
                                    : params.cert_pv_prefix + params.cert_status_pv;
+        }
+
+        if(params.only_status_pv) {
+            if(cert_id.empty())
+                return 1;
+            else
+                std::cout<<cert_id<<std::endl;
+            return 0;
         }
 
         try {
