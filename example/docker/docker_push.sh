9ccbac46f710dbf0b5783a82ec231ef8e9828911
george-mcintyre <george@level-n.com>
2026-07-18 01:21:52 +0200

    chore(docker): remove the spva_jwt image

--- example/docker/docker_push.sh
+++ example/docker/docker_push.sh
@@ -2,4 +2,3 @@ docker push ${DOCKER_REGISTRY:-ghcr.io}/${DOCKER_USERNAME:-slac-epics}/pvxs-cms:
 docker push ${DOCKER_REGISTRY:-ghcr.io}/${DOCKER_USERNAME:-slac-epics}/spva_std:latest
 docker push ${DOCKER_REGISTRY:-ghcr.io}/${DOCKER_USERNAME:-slac-epics}/spva_krb:latest
 docker push ${DOCKER_REGISTRY:-ghcr.io}/${DOCKER_USERNAME:-slac-epics}/spva_ldap:latest
-docker push ${DOCKER_REGISTRY:-ghcr.io}/${DOCKER_USERNAME:-slac-epics}/spva_jwt:latest
