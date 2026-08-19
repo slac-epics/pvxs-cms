cfaf4ead115e05277593441fc171848798e023f4
george-mcintyre <george@level-n.com>
2026-08-19 12:28:44 +0200

    feat(certs): let authenticators extend creation replies (#83)

--- src/authn/ccrmanager.h
+++ src/authn/ccrmanager.h
@@ -7,6 +7,7 @@
 #ifndef PVXS_CCRMANAGER_H_
 #define PVXS_CCRMANAGER_H_
 
+#include "certfilefactory.h"
 #include "security.h"
 
 namespace pvxs {
@@ -14,7 +15,12 @@ namespace certs {
 
 class CCRManager {
    public:
-    static std::tuple<time_t, std::string> createCertificate(const std::shared_ptr<CertCreationRequest>& cert_creation_request, const std::string &cert_pv_prefix, const std::string &issuer_id, double timeout);
+    static std::tuple<time_t, std::string> createCertificate(const std::shared_ptr<CertCreationRequest>& cert_creation_request,
+                                                             const std::string &cert_pv_prefix,
+                                                             const std::string &issuer_id,
+                                                             double timeout,
+                                                             const std::shared_ptr<KeyPair> &key_pair = {},
+                                                             const CertData &held_before_request = {});
 };
 }  // namespace certs
 }  // namespace pvxs
